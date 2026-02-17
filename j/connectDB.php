321209017468f20e743e127dc29ed3713e01754f
JakarinIntharam <66010914090@msu.ac.th>
2026-02-17 12:11:30 +0700

    Delete j directory

--- j/connectDB.php
+++
@@ -1,9 +0,0 @@
-<?php
-                $host ="localhost";  
-                $user = "root";
-                $pwd = "";
-                $DB = "4090db"; 
-                #เชื่อมต่อฐานข้อมูล
-                $conn = mysqli_connect($host,$user,$pwd,$DB) or die ("เชื่อมต่อฐานข้อมูลไม่ได้");
-                mysqli_query($conn, "SET NAMES utf8");
-?>
